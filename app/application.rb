23d231516c41a8e02c1f09d43d76a73ee1443cfe
Sophie Gu <sophieqgu@gmail.com>
2019-07-18 05:38:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,18 +11,21 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    
     elsif req.path.match(/cart/)
       
       if @@cart.empty?
-        resp.write ""
+        resp.write "Your cart is empty"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
         end 
       end
+      
     else
       resp.write "Path Not Found"
     end
