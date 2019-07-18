589195ad7df4dba3d7766434be9db93f23dc1f6f
Sophie Gu <sophieqgu@gmail.com>
2019-07-18 05:39:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,10 @@ class Application
           resp.write "#{item}\n"
         end 
       end
-      
+    
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+    
     else
       resp.write "Path Not Found"
     end
